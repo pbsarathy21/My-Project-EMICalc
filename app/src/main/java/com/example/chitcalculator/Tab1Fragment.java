6f068b07ebe2d6256aacb9cce30e487182e76d4c
pbsarathy21 <pbsarathy21@gmail.com>
2019-04-07 11:01:29 +0530

    Commited by partha

--- app/src/main/java/com/example/chitcalculator/Tab1Fragment.java
+++ app/src/main/java/com/example/chitcalculator/Tab1Fragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 
 /**
@@ -17,6 +19,7 @@ public class Tab1Fragment extends Fragment {
 
     EditText loanAmount,interestAmount,daysEditText;
     Button calculateInterest;
+    TextView result;
 
 
     public Tab1Fragment() {
@@ -25,13 +28,76 @@ public class Tab1Fragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
 
 
+        loanAmount = view.findViewById(R.id.loanAmount);
+        interestAmount= view.findViewById(R.id.interestAmount);
+        daysEditText = view.findViewById(R.id.daysEditText);
 
+        calculateInterest = view.findViewById(R.id.calculateInterest);
+
+        result = view.findViewById(R.id.interest_result);
+
+        result.setVisibility(View.GONE);
+
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String amount = loanAmount.getText().toString().trim();
+                String interest = interestAmount.getText().toString().trim();
+                String days = daysEditText.getText().toString().trim();
+
+                boolean valid = checkValidation(amount, interest, days);
+
+                if (valid)
+                    calculateInterest(amount, interest, days);
+            }
+        });
+
+        return view;
+    }
+
+    private void calculateInterest(String amount, String interest, String days) {
+
+        int amt = Integer.parseInt(amount);
+        int intrst = Integer.parseInt(interest);
+        int day = Integer.parseInt(days);
+
+        double res = (amt/1000)*(intrst/3)*day;
+
+        result.setText(String.valueOf(res));
+        result.setVisibility(View.VISIBLE);
+    }
+
+    private boolean checkValidation(String amount, String interest, String days) {
+
+        if (amount.isEmpty())
+        {
+            loanAmount.setError("required");
+            loanAmount.requestFocus();
+            return false;
+        }
+
+        if (interest.isEmpty())
+        {
+            interestAmount.setError("required");
+            interestAmount.requestFocus();
+            return false;
+        }
+
+        if (days.isEmpty())
+        {
+            daysEditText.setError("required");
+            daysEditText.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 
 }
